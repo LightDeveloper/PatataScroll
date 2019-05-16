fd76668e745a3c57b441fc3ac64878abe4f56b8d
David <david@mail.com>
2019-05-16 19:54:26 +0200

    El pagecontrol se mueve según avanzo de imágenes

---
+++ PatataScroll/TinderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TinderViewController.swift
+//  PatataScroll
+//
+//  Created by Dev2 on 16/05/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class TinderViewController: UIViewController {
+
+    let peopleCount = 4
+
+    @IBOutlet weak var pagePictures: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        pagePictures.currentPage = 0
+        pagePictures.numberOfPages = peopleCount
+    }
+}
+
+extension TinderViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        debugPrint("El offset es \(scrollView.contentOffset)")
+        
+        pagePictures.currentPage = Int(scrollView.contentOffset.x / 300)
+    }
+}
